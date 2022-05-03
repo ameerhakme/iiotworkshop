0f20348f0a99a104bb71e17e88019672eb6beb0c
Ameer Hakme <79616618+ameerhakme@users.noreply.github.com>
2022-05-03 13:45:24 -0400

    Update BaseImplementationStack.ts

--- v2/base/cdk/lib/BaseImplementationStack.ts
+++ v2/base/cdk/lib/BaseImplementationStack.ts
@@ -166,20 +166,19 @@ export class BaseImplementationStack extends cdk.Stack {
     // Add core public components
     greengrassDeployment.addComponent({
       "aws.greengrass.Nucleus": {
-        componentVersion: "2.4.0"
+        componentVersion: "2.5.5"
       },
       "aws.greengrass.Cli": {
-        componentVersion: "2.4.0"
+        componentVersion: "2.5.5"
       }
     })
+
     greengrassDeployment.addComponent({
-      "aws.greengrass.LocalDebugConsole": {
-        componentVersion: "2.2.2",
-        configurationUpdate: {
-          merge: JSON.stringify({
-            httpsEnabled: "false"
-          })
-        }
+      "aws.iot.SiteWiseEdgePublisher": {
+        componentVersion: "2.1.4" 
+      },
+      "aws.iot.SiteWiseEdgeCollectorOpcua": {
+        componentVersion: "2.1.1" 
       }
     })
 
